2acbf723343aa0d66430e76e3e375774b6c8c7bd
Liudmila <kiianliudmila@gmail.com>
2024-11-05 17:34:35 +0200

    Add Task2

---
+++ Task02/Lab2/MainForm.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+using static Lab2.OpenGL;
+
+namespace Lab2
+{
+    public partial class MainForm : Form
+    {
+        private DomainUpDown horizontalCountUpDown;
+        private DomainUpDown verticalCountUpDown;
+        private GroupBox radioButtonGroup;
+        private RadioButton fillModeRadioButton;
+        private RadioButton lineModeRadioButton;
+        private RadioButton pointModeRadioButton;
+        private RenderControl render;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            //Cтворення елементів інтерфейсу для взаємодії з користувачем
+            CreateRenderControl();
+            CreateLabels();
+            CreateDomainUpDown();
+            CreateRadioButtonGroup();
+        }
+
+        // методи для створення компонентів інтерфейсу
+        private void CreateRenderControl()
+        {
+            render = new RenderControl();
+            render.Location = new System.Drawing.Point(15, 15);
+            render.Size = new System.Drawing.Size(600, 400);
+            render.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            this.Controls.Add(render);
+        }
+
+        private void CreateLabels()
+        {
+            Label tileCountLabel = new Label();
+            tileCountLabel.Text = "Tile Count:";
+            tileCountLabel.Location = new System.Drawing.Point(668, 14);
+            tileCountLabel.AutoSize = true;
+            tileCountLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            this.Controls.Add(tileCountLabel);
+
+            Label horizontalLabel = new Label();
+            horizontalLabel.Text = "Horizontal";
+            horizontalLabel.Location = new System.Drawing.Point(650, 50);
+            horizontalLabel.AutoSize = true;
+            horizontalLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            this.Controls.Add(horizontalLabel);
+
+            Label verticalLabel = new Label();
+            verticalLabel.Text = "Vertical";
+            verticalLabel.Location = new System.Drawing.Point(650, 83);
+            verticalLabel.AutoSize = true;
+            verticalLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            this.Controls.Add(verticalLabel);
+        }
+
+        private void CreateDomainUpDown()
+        {
+            horizontalCountUpDown = new DomainUpDown();
+            horizontalCountUpDown.Location = new System.Drawing.Point(737, 47);
+            horizontalCountUpDown.Size = new System.Drawing.Size(50, 27);
+            horizontalCountUpDown.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+
+            for (int i = 1; i <= 100; i++)
+                horizontalCountUpDown.Items.Add(i.ToString());
+
+            horizontalCountUpDown.SelectedItem = "1";
+            horizontalCountUpDown.ReadOnly = true;
+            horizontalCountUpDown.SelectedItemChanged += HorizontalCountUpDown_SelectedItemChanged;
+            this.Controls.Add(horizontalCountUpDown);
+
+            verticalCountUpDown = new DomainUpDown();
+            verticalCountUpDown.Location = new System.Drawing.Point(737, 80);
+            verticalCountUpDown.Size = new System.Drawing.Size(50, 27);
+            verticalCountUpDown.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+
+            for (int i = 1; i <= 100; i++)
+                verticalCountUpDown.Items.Add(i.ToString());
+
+            verticalCountUpDown.SelectedItem = "1";
+            verticalCountUpDown.ReadOnly = true;
+            verticalCountUpDown.SelectedItemChanged += VerticalCountUpDown_SelectedItemChanged;
+            this.Controls.Add(verticalCountUpDown);
+        }
+
+        private void CreateRadioButtonGroup()
+        {
+            radioButtonGroup = new GroupBox();
+            radioButtonGroup.Text = "Mods";
+            radioButtonGroup.Location = new System.Drawing.Point(653, 290);
+            radioButtonGroup.Size = new System.Drawing.Size(130, 100);
+            radioButtonGroup.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            fillModeRadioButton = new RadioButton();
+            fillModeRadioButton.Text = "Fill Mode";
+            fillModeRadioButton.Location = new System.Drawing.Point(3, 3);
+            fillModeRadioButton.AutoSize = true;
+            fillModeRadioButton.Checked = true;
+            fillModeRadioButton.CheckedChanged += new EventHandler(RadioButton_CheckedChanged);
+
+            lineModeRadioButton = new RadioButton();
+            lineModeRadioButton.Text = "Line Mode";
+            lineModeRadioButton.Location = new System.Drawing.Point(3, 33);
+            lineModeRadioButton.AutoSize = true;
+            lineModeRadioButton.CheckedChanged += new EventHandler(RadioButton_CheckedChanged);
+
+            pointModeRadioButton = new RadioButton();
+            pointModeRadioButton.Text = "Point Mode";
+            pointModeRadioButton.Location = new System.Drawing.Point(3, 63);
+            pointModeRadioButton.AutoSize = true;
+            pointModeRadioButton.CheckedChanged += new EventHandler(RadioButton_CheckedChanged);
+
+            radioButtonGroup.Controls.Add(fillModeRadioButton);
+            radioButtonGroup.Controls.Add(lineModeRadioButton);
+            radioButtonGroup.Controls.Add(pointModeRadioButton);
+            this.Controls.Add(radioButtonGroup);
+        }
+
+        // методи для відслідковування змін в компонентах
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateRenderControl();
+        }
+
+        private void HorizontalCountUpDown_SelectedItemChanged(object sender, EventArgs e)
+        {
+            UpdateRenderControl();
+        }
+
+        private void VerticalCountUpDown_SelectedItemChanged(object sender, EventArgs e)
+        {
+            UpdateRenderControl();
+        }
+
+        // метод для оновлення значень в RenderControl
+        private void UpdateRenderControl()
+        {
+            int horizontalCount = int.Parse(horizontalCountUpDown.SelectedItem.ToString());
+            int verticalCount = int.Parse(verticalCountUpDown.SelectedItem.ToString());
+
+            // перевіряємо, яка саме RadioButton вибрана
+            uint mode = lineModeRadioButton.Checked ? GL_LINE_STRIP :
+                pointModeRadioButton.Checked ? GL_POINTS : 
+                GL_TRIANGLES;
+            render.SetTileCounts(horizontalCount, verticalCount, mode);
+        }
+    }
+}
---
+++ Task02/Lab2/RenderControl/RenderControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+
+namespace Lab2
+{
+    public partial class RenderControl : OpenGL
+    {
+        Figure figure;
+        // значення за стандартом
+        private int horizontalCount = 1;
+        private int verticalCount = 1;
+        private uint renderMode = GL_TRIANGLES;
+
+        public RenderControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RenderControl_Render(object sender, EventArgs e)
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glLoadIdentity();
+
+            if (Width > Height)
+                glViewport((Width - Height) / 2, 0, Height, Height);
+            else
+                glViewport(0, (Height - Width) / 2, Width, Width);
+
+            int size = 100;
+            glOrtho(-size, +size * ((horizontalCount + verticalCount - 1) * 1.5), -size * horizontalCount*1.25, +size * verticalCount*1.25, -1, +1);
+            figure.SetFigureCenters(verticalCount, horizontalCount, renderMode);
+        }
+
+        //створення об'єкту класу Figure
+        private void RenderControl_ContextCreated(object sender, EventArgs e)
+        {
+            figure = new Figure();
+        }
+
+        // метод для оновлення значень в RenderControl та запуску  повторного малювання
+        public void SetTileCounts(int hCount, int vCount, uint rMode)
+        {
+            horizontalCount = hCount;
+            verticalCount = vCount;
+            renderMode = rMode;
+            Invalidate(true);
+        }
+    }
+}
